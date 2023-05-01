4da41664bebc3e25587f165d07cb0d28980f8ac5
Himanshu tyagi <himanshutyagi0158@gmail.com>
2023-05-01 14:04:18 +0530

    homepage update

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -1,5 +1,6 @@
 import { Link, Navigate, useSearchParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { logout } from "../Redux/AuthReducer/action";
 
 export const Admin = () => {
     const adminDetail = useSelector(store => store.authReducer.admin);
@@ -13,7 +14,11 @@ export const Admin = () => {
                 <img src="https://zeevector.com/wp-content/uploads/LOGO/Amazon-India-Logo-PNG-White2.png" alt="Amazon Logo" />
                 <Link to={"/"}>Back to main page</Link>
                 <div>
-                    admin details
+                    <img src={adminDetail.image} alt="Admin image" />
+                    <p>{adminDetail.name}</p>
+                    <div>
+                        <p onClick={() => logout()}>Sign-Out</p>
+                    </div>
                 </div>
             </nav>
         </div>
